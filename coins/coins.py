d5f8cac0f15cad8325608df37a76d1012eb889bf
zencodism <none@example.com>
2014-11-05 12:54:24 +0100

    range fix

--- coins/coins.py
+++ coins/coins.py
@@ -6,7 +6,7 @@ def check_denomination():
     n = int(desc[0])
     target = int(desc[1]) if len(desc) > 1 else 0
     target = target * target
-    T = [(9001, 0, 0)] * (target + 1) # it is always named T in denomination problems. Also, it's over nine thousand!
+    T = [(90001, 0, 0)] * (target + 1) # it is always named T in denomination problems. Also, it's over nine thousand!
     T[0] = (0, 0, 0) # how many coins, sum of normal value, sum of info value
 
     for i in range(n):
@@ -17,13 +17,13 @@ def check_denomination():
         X = x
         Y = y
         for i in range(target):
-            if T[i][0] != 9001:
+            if T[i][0] != 90001:
                 x = X + T[i][1]
                 y = Y + T[i][2]
                 tmp = x*x + y*y
                 if tmp <= target and T[i][0] + 1 < T[tmp][0]:
                     T[tmp] = (T[i][0] + 1, x, y) 
-    if T[target][0] == 9001:
+    if T[target][0] == 90001:
         print "not possible"
     else:
         print T[target][0]
